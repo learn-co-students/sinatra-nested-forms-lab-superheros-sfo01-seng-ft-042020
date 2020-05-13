31986b3626c160349606cab2e8d1545db828cd32
Don Mallory <dmm333@gmail.com>
2020-05-12 22:43:05 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,12 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get "/" do
+        erb :new
+    end
 
+    post "/teams" do
+        @the_team = params[:team]
+        erb :team
+    end
 end
